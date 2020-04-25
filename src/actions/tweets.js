bdba2ae1c3f9efeaa4620cf4d53578f9c5f81895
Saifur Rahman <safrrhmn@hotmail.com>
2020-04-25 08:20:51 -0400

    add like functions

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,8 +1,29 @@
+import { saveLikeToggle } from "../utils/api";
+
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS";
+export const TOOGLE_TWEET = "TOOGLE_TWEET";
 
 export function receiveTweets(tweets) {
   return {
     type: RECEIVE_TWEETS,
     tweets,
+  };
+}
+
+function toggleTweet({ id, authedUser, hasLiked }) {
+  return {
+    type: TOOGLE_TWEET,
+    id,
+    authedUser,
+    hasLiked,
+  };
+}
+export function handleToogleTweet(info) {
+  return (dispatch) => {
+    dispatch(toggleTweet(info));
+    return saveLikeToggle(info).catch((e) => {
+      console.warn("Error in handleTweet ", e);
+      alert("There was an error liking the tweet, Try again");
+    });
   };
 }
